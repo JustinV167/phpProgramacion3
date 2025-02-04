d1a1007b517c8e688864442360cb9023d10d892c
Justin Vegas <Justinvegas14@gmail.com>
2025-02-03 23:43:55 -0400

    asd

--- app/model/tableStruct.php
+++ app/model/tableStruct.php
@@ -4,7 +4,7 @@ class SentencesStruct
    //users
    public $usersTable = 'CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY,email VARCHAR(40) UNIQUE, name VARCHAR(20), lastname VARCHAR(20),password VARCHAR(80),status VARCHAR(15),rol VARCHAR(15),money FLOAT DEFAULT 0 );';
    public $usersCreate = 'INSERT INTO users (name, lastname, email,password,status,rol) VALUES (?,?,?,?,?,?);';
-   public $userQuery = "SELECT * FROM public.users WHERE email=? AND status='active'";
+   public $userQuery = "SELECT * FROM users WHERE email=? AND status='active'";
    public $modifyMoney="UPDATE users SET money = money + ? WHERE email = ?;";
    public $userMoney="SELECT money FROM users WHERE email=? AND status='active'";
 
@@ -16,21 +16,21 @@ class SentencesStruct
    //category
    public $categorysTable = "CREATE TABLE IF NOT EXISTS categorys (id VARCHAR(20) PRIMARY KEY,id_img INT, name VARCHAR(20) UNIQUE,status VARCHAR(20),description text);
    INSERT INTO categorys (id,id_img, name,status,description) VALUES ('fruits',1,'frutas','active','venta frutas' ),('clothes',1,'ropa','active','venta ropa' ) ON CONFLICT (name) DO NOTHING;";
-   public $allCategoryQuery = "SELECT *,(SELECT i.rute as img_rute FROM public.img_rute as i where i.id=c.id_img), (SELECT COUNT(*) FROM public.products  as p WHERE  p.id_category=c.id AND p.status!='inactive' )as n_products  FROM public.categorys as c WHERE c.status!='inactive'; ";
-   public $categoryQuery = "SELECT *,(SELECT i.rute as img_rute FROM public.img_rute as i where i.id=c.id_img), (SELECT COUNT(*) FROM public.products  as p WHERE  p.id_category=c.id AND p.status!='inactive')as n_products  FROM public.categorys as c WHERE c.status!='inactive' AND c.name ILIKE '%' || ? || '%';";
-   public $categoryId = "SELECT *  FROM public.categorys  as c WHERE  c.status!='inactive' AND c.id=?";
+   public $allCategoryQuery = "SELECT *,(SELECT i.rute as img_rute FROM img_rute as i where i.id=c.id_img), (SELECT COUNT(*) FROM products  as p WHERE  p.id_category=c.id AND p.status!='inactive' )as n_products  FROM categorys as c WHERE c.status!='inactive'; ";
+   public $categoryQuery = "SELECT *,(SELECT i.rute as img_rute FROM img_rute as i where i.id=c.id_img), (SELECT COUNT(*) FROM products  as p WHERE  p.id_category=c.id AND p.status!='inactive')as n_products  FROM categorys as c WHERE c.status!='inactive' AND c.name ILIKE '%' || ? || '%';";
+   public $categoryId = "SELECT *  FROM categorys  as c WHERE  c.status!='inactive' AND c.id=?";
    //products
    public $productsTable = "CREATE TABLE IF NOT EXISTS products (id SERIAL PRIMARY KEY,id_img INT,id_category VARCHAR(20), name VARCHAR(20) UNIQUE, description VARCHAR(60),status VARCHAR(20),price FLOAT,amount INT,id_currency INT);
    INSERT INTO products (id_img, id_category , name ,status ,price ,amount, id_currency,description) VALUES (1, 'clothes' , 'papel' ,'active' ,10 ,5, 1,'hojas de papel')  ON CONFLICT (name) DO NOTHING;";
 
    public $allProductsQuery = "SELECT *,
-      (SELECT i.rute FROM public.img_rute as i WHERE p.id_img=i.id) as rute_img
-      FROM public.products as p WHERE p.status!='inactive' AND p.id_category=?;";
+      (SELECT i.rute FROM img_rute as i WHERE p.id_img=i.id) as rute_img
+      FROM products as p WHERE p.status!='inactive' AND p.id_category=?;";
 
    public $productsQuery = "SELECT *,
-      (SELECT i.rute FROM public.img_rute as i WHERE p.id_img=i.id) as rute_img
-      FROM public.products as p WHERE p.status!='inactive' AND p.id_category=? AND p.name ILIKE '%' || ? || '%';";
-   public $modifyAmount="UPDATE public.products SET amount = amount + ? WHERE id = ?;";
+      (SELECT i.rute FROM img_rute as i WHERE p.id_img=i.id) as rute_img
+      FROM products as p WHERE p.status!='inactive' AND p.id_category=? AND p.name ILIKE '%' || ? || '%';";
+   public $modifyAmount="UPDATE products SET amount = amount + ? WHERE id = ?;";
    public $productAmount="SELECT amount FROM products WHERE id=? AND status='active'";
 
 }
