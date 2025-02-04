0af945ea32f4ebf8770bbaec28a0c50d7f73d47b
Justin Vegas <Justinvegas14@gmail.com>
2025-02-04 01:04:31 -0400

    asd

--- config.php
+++ config.php
@@ -1,13 +1,7 @@
 <?php
-    $ruta;
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
-        $ruta = 'https';
-    } else {
-        $ruta = 'http';
-    }
-    echo $ruta;
-    define('folderPath',$ruta.'://'.$_SERVER['HTTP_HOST']);
-    $urlPath=$ruta.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    
+    define('folderPath','https://'.$_SERVER['HTTP_HOST']);
+    $urlPath='htpps://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     $url=substr($urlPath,strlen(folderPath));
     define('URL',$url);
         
