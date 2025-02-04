aecfd006651581f5780ddec8c2cbb12e662ec136
Justin Vegas <Justinvegas14@gmail.com>
2025-02-03 23:47:50 -0400

    asd

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
-    define('folderPath','https://'.$_SERVER['HTTP_HOST']);
-    $urlPath='https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    $ruta;
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+        $ruta = 'https';
+    } else {
+        $ruta = 'http';
+    }
+    define('folderPath',$ruta.'://'.$_SERVER['HTTP_HOST']);
+    $urlPath=$ruta.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     $url=substr($urlPath,strlen(folderPath));
     define('URL',$url);
         
